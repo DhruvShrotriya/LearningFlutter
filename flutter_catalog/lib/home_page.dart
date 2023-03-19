7f87d46ea5c7af1805aac7349e4f9fa299af0b50
DhruvShrotriya <dhruvshrotriya03@gmail.com>
2023-03-19 12:05:00 +0530

    day 1 completed

--- flutter_catalog/lib/home_page.dart
+++ flutter_catalog/lib/home_page.dart
@@ -9,6 +9,11 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Material(
+        child: Center(
+      child: Container(
+        child: Text("This is my first app"),
+      ),
+    ));
   }
 }
